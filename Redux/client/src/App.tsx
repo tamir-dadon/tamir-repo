2b3099187e2ea261b886f1bb93f4d0307aa08826
Tamir <Tamir.dadon63@gmail.com>
2022-08-16 18:37:02 +0300

    Update 2

--- Redux/client/src/App.tsx
+++ Redux/client/src/App.tsx
@@ -1,58 +1,23 @@
-import React from "react";
+import "./App.css";
 import logo from "./logo.svg";
 import Text from "./features/Text/Text";
-import "./App.css";
 import Joke from "./features/Joke/Joke";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Login from "./features/Login/Login";
+import Home from "./features/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Text />
-        <Joke />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/Home" element={<Home />}>
+            <Route path=":Joke" element={<Joke />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Redux/client/src/features/Login/Login.tsx
@@ -0,0 +1,8 @@
+import axios from "axios"
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
